10e223c908b37e8d55fa4a2906d74172d4edb592
wellsilver <wellsilver2016@gmail.com>
2022-01-26 15:48:35 -0500

    Update prim.sh

--- src/prim.sh
+++ src/prim.sh
@@ -1,4 +1,3 @@
 # Similar to EZserver lets you download/run jars and stuff
 
 #locals lol
-export SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd ) #lol batch does this automaticly, windows better this will be very annoying in the future
